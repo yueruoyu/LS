d24a799d1c335248743f268011f6bd62f874d2b8
Roy Yue <yueruoyu@gmail.com>
2023-03-04 07:34:09 -0600

    03042023

--- RB101/lesson_2/calculator.rb
+++ RB101/lesson_2/calculator.rb
@@ -4,78 +4,100 @@
 # output the results
 
 # answer = Kernal.gets()
+require 'yaml'
+MESSAGES = YAML.load_file('calculator_messages.yml')
+LANGUAGE = 'en'
 
 def prompt(message)
-	Kernel.puts("=> #{message}")
+	Kernel.puts("=> #{MESSAGES[LANGUAGE][message]}")
 end
 
 def valid_number(number)
 	number.to_i.to_s == number
 end
 
-prompt("Welcome to Calculator!")
+def operation_to_message(op)
+	action = case op
+						when "1"
+							'Adding'
+						when "2"
+							'Subtracting'
+						when "3"
+							"Multiplying"
+						when "4"
+							"Dividing"
+						end
+	action
+end
 
-loop do
-	prompt("What's your name?")
-	username = Kernel.gets().chomp()
-	if username.empty?
-		prompt("Hmm...Please make sure to provide your name:")
-	else
+prompt('welcome')
+
+	loop do
+		username = Kernel.gets().chomp()
+		if username.empty?
+			prompt('vaild_name')
+		else
+			break
+		end
+	end	
+
+loop do 
+	number1 = ''
+	loop do
+		prompt('ask_first_number')
+		number1 = Kernel.gets().chomp()
+		if valid_number(number1)
+			break
+		else
+			prompt('valid_number')
+		end
+	end
+
+	number2 = ''
+	loop do
+		prompt('ask_second_number')
+		number2 = Kernel.gets().chomp()
+		if valid_number(number2)
+			break
+		else
+			prompt('valid_number')
+		end
+	end
+
+	
+
+	operator = ''
+	prompt('operator_message')
+	loop do
+		operator = Kernel.gets().chomp()
+		if %w(1 2 3 4).include?(operator)
+			break
+		else
+			prompt('valid_operator')
+		end
+	end
+
+	Kernel.puts("#{operation_to_message(operator)} the two numbers...")
+
+	result = case operator
+					when '1'
+						number1.to_i + number2.to_i
+					when '2'
+						number1.to_i - number2.to_i
+					when '3'
+						number1.to_i * number2.to_i
+					when '4'
+						number1.to_f / number2.to_f
+	end
+	
+	prompt("calculation_result")
+	Kernel.puts(result)
+
+	prompt('calculate_again')
+	loop_again = Kernel.gets().chomp()
+	if loop_again.downcase != 'y'
 		break
 	end
-end
 
-number1 = ''
-loop do
-	prompt("What's the first number?")
-	number1 = Kernel.gets().chomp()
-	if valid_number(number1)
-		break
-	else
-		prompt("Please make sure to input a valid number:")
-	end
-end
-
-number2 = ''
-loop do
-	prompt("What's the second number?")
-	number2 = Kernel.gets().chomp()
-	if valid_number(number2)
-		break
-	else
-		prompt("Please make sure to input a valid number:")
-	end
-end
-
-operator_message =<<-TEXT
-	What's the operation? 
-	1) add
-	2) subtract
-	3) multiply
-	4) divide
-TEXT
-
-operator = ''
-prompt(operator_message)
-loop do
-	operator = Kernel.gets().chomp()
-	if %w(1 2 3 4).include?(operator)
-		break
-	else
-		prompt("Please input number 1/2/3/4 only")
-	end
-end
-
-result = case operator
-				when '1'
-					number1.to_i + number2.to_i
-				when '2'
-					number1.to_i - number2.to_i
-				when '3'
-					number1.to_i * number2.to_i
-				when '4'
-					number1.to_f / number2.to_f
-end
-
-prompt("The result is #{result}")
+end 
 
---
+++ RB101/lesson_2/loan_calculator.rb
@@ -0,0 +1,58 @@
+#Mortgage/Car Loan Calculator
+
+Kernel.puts("Welcome to Mortage/Car Loan Calculator. Please enter your name:")
+
+def number(num)
+	num.to_i.to_s == num || num.to_f.to_s == num
+end 
+
+loop do 
+	name = Kernel.gets().chomp()
+	if !name.empty?
+		break
+	end
+	Kernel.puts("Please enter a valid name!") 
+end 
+
+Kernel.puts("Please enter your loan amount")
+loan_amount_input = ''
+loop do
+	loan_amount_input = Kernel.gets().chomp()
+	if number(loan_amount_input) 
+		break
+	end 
+	Kernel.puts("Please enter a vaid loan amount")
+end
+
+loan_amount = loan_amount_input.to_f
+
+Kernel.puts("Please enter your annual percentage rate(APR, in percentage)")
+annual_percentage_rate = ''
+loop do
+	annual_percentage_rate = Kernel.gets().chomp()
+	if number(annual_percentage_rate) 
+		break
+	end 
+	Kernel.puts("Please enter a vaid APR")
+end
+
+monthly_interst_rate = annual_percentage_rate.to_f / 1200
+
+loan_years = ''
+Kernel.puts("Please enter your loan duration (in years)")
+loop do
+	loan_years = Kernel.gets().chomp()
+	if number(loan_years) 
+		break
+	end 
+	Kernel.puts("Please enter a vaid loan duration")
+end
+
+loan_months = loan_years.to_f * 12
+
+monthly_payment = loan_amount * (monthly_interst_rate / (1 - (1 + monthly_interst_rate)**(-loan_months)))
+
+Kernel.puts("Your monthly payment is #{monthly_payment}")
+
+
+
